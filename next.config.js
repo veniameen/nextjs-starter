1475a67b7bebe4c387ed2e1edfce7c3bb59f266c
Veniamin Ryadnov <veniamin.ryadnov@gmail.com>
2022-04-06 02:07:53 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const StylelintPlugin = require("stylelint-webpack-plugin"); // line to add
+const StylelintPlugin = require('stylelint-webpack-plugin') // line to add
 module.exports = {
   reactStrictMode: true,
-      
+
   // lines to add
   webpack: (config, options) => {
-    config.plugins.push(new StylelintPlugin());
-    return config;
+    config.plugins.push(new StylelintPlugin())
+    return config
   },
-
-};
+}
